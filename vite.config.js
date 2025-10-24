1a3b4aab48d886053d2b934863665ac425b1653f
jasim ek <jasimek50@gmail.com>
2025-10-24 19:07:42 +0530

    https url issue fix

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,16 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name].[hash][extname]',
+                chunkFileNames: 'assets/[name].[hash].js',
+                entryFileNames: 'assets/[name].[hash].js',
+            },
+        },
+    },
+    server: {
+        https: false,
+    },
 });
